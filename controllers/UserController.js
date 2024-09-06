d8187c4f8d625821c8c54c57ecc3079e7b30bb1a
Kernelogy <kernelogy@gmail.com>
2024-09-06 15:12:36 +0530

    Orm Part 1

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,6 @@
 const UserModel = require("../models/UserModel")
+const AddressModel = require("../models/AddressModel")
+const ContactModel = require("../models/ContactModel")
 
 exports.insert = [(req, res)=>{
     const User = new UserModel({
@@ -16,8 +18,75 @@ exports.insert = [(req, res)=>{
     })
 }]
 
+exports.insertWithContacts = [async (req, res)=>{
+    const rContacts = req.body.contacts
+    let savedContactIds = []
+
+    // rContacts.map((e)=>{
+    for(let i=0; i<rContacts.length; i++){
+        const e = rContacts[i]
+        const contact = new ContactModel({
+            data: e.data,
+            type: e.type
+        })
+        await contact.save()
+        .then((savedContact)=>{
+            savedContactIds.push(savedContact._id)
+        })
+    }
+    // })
+
+    const User = new UserModel({
+        username: req.body.username,
+        contacts: savedContactIds, 
+        password: req.body.password
+    })
+    User.save()
+    .then((savedObject)=>{
+        res.send(savedObject)
+    })
+    .catch((err)=>{
+        res.send(err)
+    })
+}]
+
+
+exports.insertWithAddress = [(req, res)=>{
+    const address = new AddressModel({
+        plotNo: req.body.address.plotNo,
+        street: req.body.address.street,
+        city: req.body.address.city,
+        landmark: req.body.address.landmark,
+        pincode: req.body.address.pincode,
+        state: req.body.address.state,
+        country: req.body.address.country
+    })
+
+    address.save()
+    .then((savedAddress)=>{
+        const User = new UserModel({
+            username: req.body.username,
+            email: req.body.email,
+            contact: req.body.contact, 
+            password: req.body.password,
+            address: savedAddress._id
+        })
+        User.save()
+        .then((savedObject)=>{
+            res.send(savedObject)
+        })
+        .catch((err)=>{
+            res.send(err)
+        })    
+    })
+
+    
+}]
+
 exports.list = [(req,res)=>{
-    UserModel.find() //returns multiple objects as an array
+    // UserModel.find() //returns multiple objects as an array
+    // UserModel.find().populate("address")
+    UserModel.find().populate(["address", "contacts"])
     .then((users)=>{
         res.send(users)
     })
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -4,8 +4,11 @@ const router = express.Router()
 const UserController = require("../controllers/UserController")  
 
 router.post("/user/insert", UserController.insert) 
+router.post("/user/insertWithAddress", UserController.insertWithAddress) 
+router.post("/user/insertWithContacts", UserController.insertWithContacts) 
 router.get("/user/list", UserController.list)
 router.post("/user/login", UserController.login)
 router.delete("/user/delete/:id", UserController.delete)
+
 
 module.exports = router
